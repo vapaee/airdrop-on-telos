a04b41e041a8d7c6186d26a46e7912e77e55d508
Viterbo Rodriguez <Viterbo@users.noreply.github.com>
2019-03-23 20:41:26 -0300

    Update perform_airdrop.js

--- perform_airdrop.js
+++ perform_airdrop.js
@@ -1,28 +1,43 @@
+// This airdrop is based on the first Telos orignal snapshot and the new claim action developped for this goal.
+// This script reeads the snapshot from the local csv_file and performs a call to the claim action of the smart contract.
+// To run a simulation set simulate = true. This will not execute any cleos command.
+
 var fs = require('fs');
 var sys = require('sys')
 var exec = require('child_process').exec;
 let csv = require('fast-csv');
 
-// variables -----------------------
+// Constants -----------------------
+var simulate = true; // if true, the script will not execute the actual commands to the blockchain
+var csv_file = "6MSnapshotWithBalances.csv";
+var contract_name = "vapaeetokens";
+var ram_payer = "vapaeetokens";
 var local = "cleos";
 var telostest = "cleos --url https://testnet.telos.caleos.io";
 var telosmain = "cleos --url https://telos.eos.barcelona";
-var cleos = telosmain;
 var milliseconds = 0;
 var delay = 0;
-var command;
-// ---
+
+// --- which blockchain will you use ?
+var cleos = telosmain;
+
+// --- cropping the target accounts (modify them to adjust your needs)
+var min = 0; // receiver must have at least min TLOS to be added in this airdrop
+var max = 0; // receiver must have less than max TLOS to be added in this airdrop (0 means no effect)
+var cap = 0; // receiver airdrop will be capped at cap
+
+
+// -- auxiliar variables (don't modify)
 var total = 0;
 var accounts = 0;
 var capped = 0;
 var average = 0;
-var min = 0;
-var max = 999999999;
-var cap = 0;
+var command;
 
 // -------------
-min = 100;
-max = 1000;
+// the following example will perform an aidrop where only users between [100] and (1000)
+// will receive an amount of tokens equal to the amount of TLOS but capped at 40K.
+// min = 100;
 // cap = 40000;
 
 var commands = [];
@@ -49,7 +64,7 @@ function process_snapshot() {
         }
     });
 }
-var stream = fs.createReadStream("6MSnapshotWithBalances.csv");
+var stream = fs.createReadStream(csv_file);
 csv.fromStream(stream, {headers : true})
     .on("data", function(data) {
         data.balance = parseFloat(data.balance);
@@ -58,9 +73,7 @@ csv.fromStream(stream, {headers : true})
             capped++;
         }
         if (data.balance < min) return;
-        if (data.balance >= max) return;
-        // if (accounts > 3) return;
-        // if (0 < max) if (data.balance > max) return;
+        if (0 < max) if (data.balance >= max) return;
 
         total += data.balance;
         accounts++;
@@ -69,7 +82,7 @@ csv.fromStream(stream, {headers : true})
         account = data.account;
         balance = Math.floor(data.balance * 10000);
         // push to local net ----
-        command = cleos + " push action vapaeetokens claim '[\""+account+"\",\"CNT\",\"vapaeetokens\"]' -p vapaeetokens@active";
+        command = cleos + " push action " + contract_name + " claim '[\""+account+"\",\"CNT\",\"" + ram_payer + "\"]' -p " + ram_payer + "@active";
         commands.push(command);
     })
     .on("end", function() {
@@ -77,11 +90,12 @@ csv.fromStream(stream, {headers : true})
         console.log("-----------------------");
         console.log("cap:      ", cap);
         console.log("min:      ", min);
+        console.log("max:      ", max);
         console.log("-----------------------");
         console.log("total:    ", total);
         console.log("average:  ", average);
         console.log("accounts: ", accounts);
         console.log("capped:   ", capped);
-        process_snapshot();
+        if (!simulate) process_snapshot();
     });
 
